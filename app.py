dd9a522c0676c39436df028e8de3049d67a0e30b
ehuntober <ehuntercrypto@gmail.com>
2022-09-09 14:16:44 +0000

    new way

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from application import app
+from flask import Flask
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
